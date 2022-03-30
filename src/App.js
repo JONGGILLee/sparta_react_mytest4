68299f3fa8130f91efcbf62ace42df1431e84d1c
JONGGIL LEE <brianlee2k@naver.com>
2022-03-30 17:44:20 +0900

    3-4강 리액트에서 라우팅 처리하기 : {Link}  <Link to="/"> 이용하여 링크 이동

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import "./App.css";
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import styled from "styled-components";
 import Nemo from "./Nemo";
 import Button from "./Button";
 import Circle from "./Circle";
 import Header from "./Header";
 import Cat from "./Cat";
+import Dog from "./Dog";
 
 const list = ["산책하기", "코딩공부하기", "등산하기"];
 
 const App = () => {
   return (
     <div className="App">
+      <div>
+        <Link to="/">Header로 가기</Link>
+        <Link to="/Cat">Cat으로 가기</Link>
+        <Link to="/Dog">Dog으로 가기</Link>
+      </div>
+
       <Route path="/" exact>
         <Header />
       </Route>
-      <Route path="/Cat/:cat_name" exact component={Cat}>
+      <Route path="/Cat" exact component={Cat}>
         {/* cat_name 은 동적으로 바꿀거야 -> 동적 라우팅 */}
         {/* <Cat /> */}
       </Route>
+      <Route path="/Dog" exact component={Dog}></Route>
       <Nemo />
       <Button list={list} />
       <Circle />
