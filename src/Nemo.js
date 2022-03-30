c4bef5c13ee69029b56681b0d158b4c1ccf372b2
JONGGIL LEE <brianlee2k@naver.com>
2022-03-30 09:09:38 +0900

    2-12강 함수형 컴포넌트에서 State 다루기. useState라는 리액트 훅 이용해서 state 넣어줄 값, 그 값을 바꿔줄 함수 만듦

--- src/Nemo.js
+++ src/Nemo.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Nemo = (props) => {
-  const nemo_count = Array.from({ length: 5 }, (v, i) => i);
+  const [count, setCount] = React.useState(3);
+  //   count 를 변경하는 함수가 setCount에 들어간다. 그리고 state 에 들어가는 초기값3을 useState(3)에 넣어준다.
+  console.log(count);
+  const nemo_count = Array.from({ length: count }, (v, i) => i);
+
+  const addNemo = () => {
+    setCount(count + 1);
+  };
+  const deleteNemo = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      window.alert("네모가 없습니다.");
+    }
+  };
+
   return (
     <>
       {nemo_count.map((n, i) => {
@@ -21,7 +36,8 @@ const Nemo = (props) => {
       })}
 
       <div>
-        <button> 추가버튼</button>
+        <button onClick={addNemo}> 추가버튼</button>
+        <button onClick={deleteNemo}> 삭제버튼</button>
       </div>
     </>
   );
