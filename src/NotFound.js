7986aa447b9693d351b1917c23c316e41b44c829
JONGGIL LEE <brianlee2k@naver.com>
2022-03-31 10:30:31 +0900

    3-6강. 라우팅 더 꼼꼼하게 <Switch> 이용하여 NotFound.js 페이지2

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const NotFound = (props) => {
-  return <h1>올바른 주소가 아닙니다!0</h1>;
+  const history = useHistory();
+
+  return (
+    <>
+      <h1>올바른 주소가 아닙니다!~</h1>
+      <button
+        onClick={() => {
+          history.push("/");
+        }}
+      >
+        홈으로 돌아가기
+      </button>
+    </>
+  );
 };
 
 export default NotFound;
