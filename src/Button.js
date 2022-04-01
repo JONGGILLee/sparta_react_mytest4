82af1edcd5d14a06e90f2bc7b4eafee92148012c
JONGGIL LEE <brianlee2k@naver.com>
2022-04-01 15:27:40 +0900

    3-11강 컴포넌트에서 리덕스 데이터 업테이트 Button.js 에서 import { useDispatch } from "react-redux" 그리고 const dispatch =useDispatch(); 그리고 dipatch(액션 객체) 액션객체는 딕셔너리로/매번 쓰기 귀찮으니 액션생성함수로

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { createDict } from "./redux/modules/dict";
 
 const Button = (props) => {
   const my_wrap = React.useRef(null);
@@ -8,12 +10,15 @@ const Button = (props) => {
 
   // const [list, setList] = React.useState(["영화관 기", "매일 책읽"]);
   const list = useSelector((state) => state.dict.list);
-
   console.log(list);
+  const dispatch = useDispatch();
 
   const addList = () => {
-    console.log(my_wrap.current.value);
-    return [...list, my_wrap.current.value];
+    // console.log(my_wrap.current.value);
+    // return [...list, my_wrap.current.value];
+
+    dispatch(createDict(my_wrap.current.value));
+    // dispatch(액션 객체) 액션 객체 어떻게 생겼어요? 딕셔너리로 {type:"",data} 매번 쓰기 귀찮으니 액션생성함수로
   };
   //   window.setTimeout(() => {
   //     // 1초 뒤에는?!
@@ -36,7 +41,7 @@ const Button = (props) => {
             console.log(my_wrap.current.value);
           }}
         ></input>
-        <button onClick={addList}>추가하기</button>
+        <button onClick={addList}>리스트 추가하기</button>
       </div>
     </>
   );
